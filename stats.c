d91ec30fd1f3371c0e241f13ce0a14477d995aad
Maximiliano Martinez <matsmaco@gmail.com>
2024-10-03 22:53:22 -0600

    All functions has been added and correctly executed

--- stats.c
+++ stats.c
@@ -33,11 +33,88 @@ void main() {
                               114, 88,   45,  76, 123,  87,  25,  23,
                               200, 122, 150, 90,   92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};
+                                7,  87, 250, 230,  99,   3, 100,  90};                          
 
   /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
+  unsigned char media;
+  unsigned char mediana;
+  unsigned char valorMinimo;
+  unsigned char valorMaximo;
 
+  /* Statistics and Printing Functions Go Here */
+  print_array(test, SIZE);
+  sort_array(test,SIZE);
+  media = find_mean(test, SIZE);
+  mediana = find_median(test, SIZE);
+  valorMinimo = find_minimum(test, SIZE);
+  valorMaximo = find_maximum(test, SIZE);
+  print_statistics(media, mediana, valorMinimo, valorMaximo);
 }
 
 /* Add other Implementation File Code Here */
+
+void sort_array(unsigned char *arr,unsigned int length){
+  int i, j;
+  for(i=0;i<length-1;i++){
+    for(j=0;j<length-i-1;j++){
+      if(arr[j]<arr[j+1]){
+        unsigned char temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp; 
+      }
+    }
+  }
+}
+
+void print_array(unsigned char *arr, unsigned int length){
+  printf("Elementos que contiene el arreglo \n");
+  for(int i=0;i<length;i++){
+    printf("%d, ", arr[i]);
+  }
+  printf("\n");
+}
+
+unsigned char find_mean(unsigned char *arr, unsigned int length){
+  char suma = 0;
+  for(int i=0;i<length;i++){
+    suma += arr[i];
+  }
+  return suma/length;
+}
+
+unsigned char find_median(unsigned char *arr, unsigned int length){
+  int suma;
+  if(length % 2 == 0){
+    int midPos = length/2;
+    suma = arr[midPos - 1] + arr[midPos];
+    return suma/2;
+  }
+  else{
+    int midPos = length/2 - 1;
+    return arr[midPos];
+  }
+}
+
+unsigned char find_minimum(unsigned char *arr, unsigned int length){
+  unsigned char min = arr[0];
+  for(int i=0;i<length;i++){
+    if(arr[i]<min){
+      min = arr[i];
+    }
+  }
+  return min;
+}
+
+unsigned char find_maximum(unsigned char *arr, unsigned int length){
+  unsigned char max = arr[0];
+  for(int i=0;i<length;i++){
+    if(arr[i]>max){
+      max = arr[i];
+    }
+  }
+  return max;
+}
+
+void print_statistics(unsigned char media ,unsigned char mediana, unsigned char valorMinimo, unsigned char valorMaximo){
+  printf("Media: %d\n Mediana: %d\n Valor Mínimo: %d\n Valor Máximo: %d\n", media, mediana, valorMinimo, valorMaximo);
+}
--- stats.h
+++ stats.h
@@ -38,4 +38,12 @@
  */
 
 
+void print_statistics(unsigned char media ,unsigned char mediana, unsigned char valorMinimo, unsigned char valorMaximo);
+void print_array(unsigned char *arr, unsigned int length);
+unsigned char find_median(unsigned char *arr, unsigned int length);
+unsigned char find_mean(unsigned char *arr, unsigned int length);
+unsigned char find_maximum(unsigned char *arr, unsigned int length);
+unsigned char find_minimum(unsigned char *arr, unsigned int length);
+void sort_array(unsigned char *arr, unsigned int length);
+
 #endif /* __STATS_H__ */
