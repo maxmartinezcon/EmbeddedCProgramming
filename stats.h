1daf2da2e1be90289f349d62a134ede302b9f48e
Maximiliano Martinez <matsmaco@gmail.com>
2024-10-05 01:01:18 -0600

    Se han agregado todas las descripciones para la entrega final

--- stats.h
+++ stats.h
@@ -25,25 +25,90 @@
 
 
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief <Función que nos permite imprimir las medidas estadísticas>
  *
- * <Add Extended Description Here>
+ * <Se le pasará como argumentos las diferentes medidas estadísticas>
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param <media> <char sin signo la cual tiene el valor de la media o promedio del arreglo>
+ * @param <mediana> <char sin signo la cual  contiene la mediana del  arreglo>
+ * @param <valorMinimo> <char sin signo el cual contiene el valor mínimo del arreglo>
+ * @param <valorMaximo> <char sin signo el cual contiene el valor maximo del arreglo>
  *
- * @return <Add Return Informaiton here>
+ * @return <None>
  */
-
-
 void print_statistics(unsigned char media ,unsigned char mediana, unsigned char valorMinimo, unsigned char valorMaximo);
+
+/**
+ * @brief <Función que nos permitirá imprimir un arreglo de valores>
+ *
+ * <Se imprimiran los valores del arreglo uno a uno a traves de un ciclo for>
+ *
+ * @param <arr> <char sin signo el cual indica el apuntador al arreglo>
+ * @param <length> <entero sin signo la cual nos indica el tamaño del arreglo>
+
+ *
+ * @return <None>
+ */
 void print_array(unsigned char *arr, unsigned int length);
+
+/**
+ * @brief <Encuentra la mediana del arreglo proporcionado>
+ *
+ * <Se le proporcionara el apuntador del arreglo para que encuentre la mediana de los datos>
+ *
+ * @param <arr> <char sin signo el cual indica el apuntador al arreglo>
+ * @param <length> <entero sin signo la cual nos indica el tamaño del arreglo>
+ *
+ * @return <char sin signo que contendrá la mediana>
+ */
 unsigned char find_median(unsigned char *arr, unsigned int length);
+
+/**
+ * @brief <Se encontrará la media o el promedio>
+ *
+ * <Se usará un ciclo for para realizar la suma de todos los elementos y al final se dividirá entre su longitud>
+ *
+ * @param <arr> <char sin signo el cual indica el apuntador al arreglo>
+ * @param <length> <entero sin signo la cual nos indica el tamaño del arreglo>
+ *
+ * @return <char sin signo  que tiene el promedio de los valores del arreglo>
+ */
 unsigned char find_mean(unsigned char *arr, unsigned int length);
+
+/**
+ * @brief <Función para encontrar el valor máximo>
+ *
+ * <Se buscara el valor máximo dado el arreglo>
+ *
+ * @param <arr> <char sin signo el cual indica el apuntador al arreglo>
+ * @param <length> <entero sin signo la cual nos indica el tamaño del arreglo>
+ *
+ * @return <un char sin signo que será el valor máximo del arreglo>
+ */
 unsigned char find_maximum(unsigned char *arr, unsigned int length);
+
+/**
+ * @brief <Función para encontrar el valor mínimo>
+ *
+ * <Se buscara el valor mínimo dado el arreglo>
+ *
+ * @param <arr> <char sin signo el cual indica el apuntador al arreglo>
+ * @param <length> <entero sin signo la cual nos indica el tamaño del arreglo>
+ *
+ * @return <un char sin signo que será el valor mínimo del arreglo>
+ */
 unsigned char find_minimum(unsigned char *arr, unsigned int length);
+
+/**
+ * @brief <Función para ordenar un arreglo de manera descendente>
+ *
+ * <Se utiliziran dos ciclos for tales que nos permitirán ordenar un arreglo del valor mayor al menor >
+ *
+ * @param <arr> <char sin signo el cual indica el apuntador al arreglo>
+ * @param <length> <entero sin signo la cual nos indica el tamaño del arreglo>
+ *
+ * @return <None>
+ */
 void sort_array(unsigned char *arr, unsigned int length);
 
 #endif /* __STATS_H__ */
